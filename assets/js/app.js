2376cc0e9bbed7cf35b16d36eed5c8e388d1cbed
Justin Hobbs <justinhobbs9108@gmail.com>
2019-10-09 12:46:00 -0400

    Updated stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,38 @@ function handleSubmitIndividual() {
     buildRookieStats(rookie);
 }
 
+
 function buildRookieStats(rookie) {
-    var url = 
+    var url = "/rookie";
+    d3.json(url).then(function(response) {
+        console.log(response);
+        
+        var filteredRookie = response.filter(function(item) {
+            return item["Name"] === filteredRookie;
+        });
+        Rookie = filteredRookie[0];
+        console.log(`Rookie is ${Object.getOwnPropertyNames(PokemonC)}`)
+        var RookieStats = {
+            Player: Rookie["Player"], 
+            Position: Rookie["POS"], 
+            Height: Rookie['Height'], 
+            Weight: Rookie["Weight (LBS)"], 
+            Rookie: Rookie["Team"],
+        console.log(rookieStats);
+
+        var trace = {
+            x: Object.getOwnPropertyNames(RookieStats),
+            y: Object.values(RookieStats),
+            name: Rookie["Player"],
+            type: "bar"
+        }
+
+        var data = [trace];
+        var layout = { 
+            title: "RookieStats"
+        };
+        Plotly.newPlot("IndividualStats", data, layout);
+        console.log(Object.values(RookieStats));
+    });
 }
+d3.select("#SubmitRookie").on("click", handleSubmitIndividual);
